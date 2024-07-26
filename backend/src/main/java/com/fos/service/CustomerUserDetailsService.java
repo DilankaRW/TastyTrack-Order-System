c3d50060d02770a8427beb0df029c65baf53c16d
VL.dilankarw <dilankarashmika10@gmail.com>
2024-07-26 23:59:52 +0530

    add Commit

---
+++ backend/src/main/java/com/fos/service/CustomerUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.fos.service;
+
+import com.fos.model.User;
+import com.fos.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        if (user != null){
+            throw new UsernameNotFoundException("user not found email "+username);
+        }
+        return null;
+    }
+}
