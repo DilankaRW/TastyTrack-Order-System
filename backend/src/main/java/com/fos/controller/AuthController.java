f07aeeb937e5aa763fcf148fc4defc1cf7e1329b
VL.dilankarw <dilankarashmika10@gmail.com>
2024-07-28 15:03:42 +0530

    Update Commit

--- backend/src/main/java/com/fos/controller/AuthController.java
+++ backend/src/main/java/com/fos/controller/AuthController.java
@@ -2,22 +2,29 @@ package com.fos.controller;
 
 import com.fos.config.JwtProvider;
 import com.fos.model.Cart;
+import com.fos.model.USER_ROLE;
 import com.fos.model.User;
 import com.fos.repository.CartRepository;
 import com.fos.repository.UserRepository;
+import com.fos.request.LoginRequest;
 import com.fos.response.AuthResponse;
 import com.fos.service.CustomerUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/auth")
@@ -64,5 +71,38 @@ public class AuthController {
         authResponse.setRole(savedUser.getRole());
 
         return new ResponseEntity<>(authResponse, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<AuthResponse> singnin(@RequestBody LoginRequest req){
+
+        String username = req.getEmail();
+        String password = req.getPassword();
+
+        Authentication authentication = authenticate(username, password);
+
+        Collection<? extends GrantedAuthority>authorities=authentication.getAuthorities();
+        String role = authorities.isEmpty()?null:authorities.iterator().next().getAuthority();
+
+        String jwt = jwtProvider.generateToken(authentication);
+
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setJwt(jwt);
+        authResponse.setMessage("Register success");
+        authResponse.setRole(USER_ROLE.valueOf(role));
+
+        return new ResponseEntity<>(authResponse, HttpStatus.OK);
+    }
+
+    private Authentication authenticate(String username, String password) {
+        UserDetails userDetails = customerUserDetailsService.loadUserByUsername(username);
+
+        if (userDetails == null){
+            throw new BadCredentialsException("invalid username.");
+        }
+        if (!passwordEncoder.matches(password, userDetails.getPassword())){
+            throw new BadCredentialsException("invalid username.");
+        }
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
     }
 }
