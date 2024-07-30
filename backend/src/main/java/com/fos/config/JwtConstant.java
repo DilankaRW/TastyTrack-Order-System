7a212d6642f2678d8b06cbb4ba0f87340d40a10c
VL.dilankarw <dilankarashmika10@gmail.com>
2024-07-30 23:58:13 +0530

    Add Advice

--- backend/src/main/java/com/fos/config/JwtConstant.java
+++ backend/src/main/java/com/fos/config/JwtConstant.java
@@ -1,6 +1,6 @@
 package com.fos.config;
 
 public class JwtConstant {
-    public static final String SECRET_KEY = "dfsioh4rfwl334nlj33fhryslcmgthdwqmfg";
+    public static final String SECRET_KEY = "dfsioh4rfwl334nlj33fhryslcmgthdwqmfg"; // Ensure SECRET_KEY is at least 32 characters (256 bits)
     public static final String JWT_HEADER = "Authorization";
 }
