5cb035b3250395dffb3c6d2e53c636332587f5a7
VL.dilankarw <dilankarashmika10@gmail.com>
2024-07-31 10:56:12 +0530

    add HomeController

---
+++ backend/src/main/java/com/fos/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.fos.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    public ResponseEntity<String> HomeController(){
+        return new ResponseEntity<>("Welcome to food delivery project", HttpStatus.OK);
+    }
+}
