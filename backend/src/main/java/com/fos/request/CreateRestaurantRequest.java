c3e7e5253da466a095d26faf4271bcab86562cea
VL.dilankarw <dilankarashmika10@gmail.com>
2024-08-03 16:08:40 +0530

    Add Restaurant

---
+++ backend/src/main/java/com/fos/request/CreateRestaurantRequest.java
@@ -0,0 +1,19 @@
+package com.fos.request;
+
+import com.fos.model.Address;
+import com.fos.model.ContactInformation;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class CreateRestaurantRequest {
+    private Long id;
+    private String name;
+    private String description;
+    private String cuisineType;
+    private Address address;
+    private ContactInformation contactInformation;
+    private String openingHours;
+    private List<String> images;
+}
