7363489ee916ba93ddb9d63fbb4f5b04f86b46ff
VL.dilankarw <dilankarashmika10@gmail.com>
2024-08-04 16:59:39 +0530

    AdminRestaurantController - createRestaurant

---
+++ backend/src/main/java/com/fos/controller/AdminRestaurantController.java
@@ -0,0 +1,30 @@
+package com.fos.controller;
+
+import com.fos.model.Restaurant;
+import com.fos.model.User;
+import com.fos.request.CreateRestaurantRequest;
+import com.fos.service.RestaurantService;
+import com.fos.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/admin/restaurants")
+public class AdminRestaurantController {
+    @Autowired
+    private RestaurantService restaurantService;
+    @Autowired
+    private UserService userService;
+    @PostMapping()
+    public ResponseEntity<Restaurant> createRestaurant(
+            @RequestBody CreateRestaurantRequest req,
+            @RequestHeader("Authorization") String jwt
+            ) throws Exception {
+        User user = userService.findUserByJwtToken(jwt);
+
+        Restaurant restaurant = restaurantService.createRestaurant(req,user);
+        return new ResponseEntity<>(restaurant, HttpStatus.CREATED);
+    }
+}
