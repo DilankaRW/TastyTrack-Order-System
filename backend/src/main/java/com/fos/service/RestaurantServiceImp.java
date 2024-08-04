a871e904e39cc012fac9633b3963a32c6902e295
VL.dilankarw <dilankarashmika10@gmail.com>
2024-08-04 16:42:47 +0530

    RestaurantService

--- backend/src/main/java/com/fos/service/RestaurantServiceImp.java
+++ backend/src/main/java/com/fos/service/RestaurantServiceImp.java
@@ -6,12 +6,15 @@ import com.fos.model.Restaurant;
 import com.fos.model.User;
 import com.fos.repository.AddressRepository;
 import com.fos.repository.RestaurantRepository;
+import com.fos.repository.UserRepository;
 import com.fos.request.CreateRestaurantRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class RestaurantServiceImp implements RestaurantService{
     @Autowired
@@ -21,7 +24,7 @@ public class RestaurantServiceImp implements RestaurantService{
     private AddressRepository addressRepository;
 
     @Autowired
-    private UserService userService;
+    private UserRepository userRepository;
 
     @Override
     public Restaurant createRestaurant(CreateRestaurantRequest req, User user) {
@@ -55,41 +58,67 @@ public class RestaurantServiceImp implements RestaurantService{
         if (restaurant.getName()!=null){
             restaurant.setName(updatedRestaurant.getName());
         }
-        return null;
+        return restaurantRepository.save(restaurant);
     }
 
     @Override
     public void deleteRestaurant(Long restaurantId) throws Exception {
-
+        Restaurant restaurant = findRestaurantById(restaurantId);
+        restaurantRepository.delete(restaurant);
     }
 
     @Override
     public List<Restaurant> getAllRestaurant() {
-        return null;
+        return restaurantRepository.findAll();
     }
 
     @Override
-    public List<Restaurant> searchRestaurant() {
-        return null;
+    public List<Restaurant> searchRestaurant(String keyword) {
+        return restaurantRepository.findBySearchQuery(keyword);
     }
 
     @Override
     public Restaurant findRestaurantById(Long id) throws Exception {
-        return null;
+        Optional<Restaurant> opt = restaurantRepository.findById(id);
+
+        if (opt.isEmpty()){
+            throw new Exception("Restaurant not found with id " + id);
+        }
+        return opt.get();
     }
 
     @Override
     public Restaurant getRestaurantByUserId(Long userId) throws Exception {
-        return null;
+        Restaurant restaurant = restaurantRepository.findByOwnerId(userId);
+        if (restaurant == null){
+            throw new Exception("Restaurant not found with owner id " + userId);
+        }
+        return restaurant;
     }
 
     @Override
     public RestaurantDto addToFavorites(Long restaurantId, User user) throws Exception {
-        return null;
+        Restaurant restaurant = findRestaurantById(restaurantId);
+
+        RestaurantDto dto = new RestaurantDto();
+        dto.setDescription(restaurant.getDescription());
+        dto.setImages(restaurant.getImages());
+        dto.setTitle(restaurant.getName());
+        dto.setId(restaurantId);
+
+        if (user.getFavorites().contains(dto)){
+            user.getFavorites().remove(dto);
+        }
+        else user.getFavorites().add(dto);
+
+        userRepository.save(user)
+        return dto;
     }
 
     @Override
     public Restaurant updateRestaurantStatus(Long id) throws Exception {
-        return null;
+        Restaurant restaurant = findRestaurantById(id);
+        restaurant.setOpen(!restaurant.isOpen());
+        return restaurantRepository.save(restaurant);
     }
 }
