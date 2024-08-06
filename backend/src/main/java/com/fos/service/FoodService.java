99a59d7de20b59fb1ef51b71df9e99d7ddaf0172
VL.dilankarw <dilankarashmika10@gmail.com>
2024-08-06 23:14:23 +0530

    FoodService

--- backend/src/main/java/com/fos/service/FoodService.java
+++ backend/src/main/java/com/fos/service/FoodService.java
@@ -3,7 +3,25 @@ package com.fos.service;
 import com.fos.model.Category;
 import com.fos.model.Food;
 import com.fos.model.Restaurant;
+import com.fos.request.CreateFoodRequest;
+
+import java.util.List;
 
 public interface FoodService {
     public Food createFood(CreateFoodRequest req, Category category, Restaurant restaurant);
+
+    void deleteFood(Long foodId) throws Exception;
+
+    public List<Food> getRestaurantsFood(Long restaurantId,
+                                         boolean isVegitarian,
+                                         boolean isNonveg,
+                                         boolean isSeasonal,
+                                         String foodCategory
+    );
+
+    public List<Food> searchFood(String keyword);
+
+    public Food findFoodById(Long foodId)throws Exception;
+
+    public Food updateAvailabilityStatus(Long foodId) throws Exception;
 }
