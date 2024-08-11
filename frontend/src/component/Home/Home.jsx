499964a4fe7f1496f05761d57a4d4dd282ccfd2b
VL.dilankarw <dilankarashmika10@gmail.com>
2024-08-11 23:29:09 +0530

    Home Banner

---
+++ frontend/src/component/Home/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Home.css"
+
+const Home = () => {
+  return (
+    <div className=''>
+      <section className='banner -z-50 relative flex flex-col justify-center items-center'>
+        <div className='w-[50vm] z-10 text-center'>
+            <p className='text-2xl lg:text-6xl font-bold z-10 py-5'>TastyTrack</p>
+            <p className='z-10 text-gray-300 text-xl lg:text-4xl'>Taste the Convenience: Food, Fast and Delivered.</p>
+        </div>
+        <div className='cover absolute top-0 left-0 right-0'>
+
+        </div>
+        <div className='fadout'>
+
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Home
