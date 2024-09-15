8c4f915642001cc312a536695311f2c2e37a54d9
VL.dilankarw <dilankarashmika10@gmail.com>
2024-09-15 23:56:46 +0530

    Register Form

--- frontend/src/component/Auth/RegisterForm.jsx
+++ frontend/src/component/Auth/RegisterForm.jsx
@@ -1,60 +1,92 @@
-import { Button, TextField, Typography } from '@mui/material'
-import { Field, Form, Formik } from 'formik'
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Field, Form, Formik } from "formik";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const initialValues={
-  fullName:"",
-  email:"",
-  password:"",
-  role:"ROLE_CUSTOMER"
-}
+const initialValues = {
+  fullName: "",
+  email: "",
+  password: "",
+  role: "ROLE_CUSTOMER",
+};
 export const RegisterForm = () => {
-  const navigate=useNavigate()
-  const handleSubmit=()=>{
-
-  }
+  const navigate = useNavigate();
+  const handleSubmit = (values) => {
+    console.log("form values",values)
+  };
   return (
     <div>
-      <Typography variant='h5' className='text-center'>
+      <Typography variant="h5" className="text-center">
         Register
       </Typography>
       <Formik onSubmit={handleSubmit} initialValues={initialValues}>
         <Form>
           <Field
-          as={TextField}
-          name="fullName"
-          label="full name"
-          fullWidth
-          variant="outlined"
-          margin="normal"
+            as={TextField}
+            name="fullName"
+            label="full name"
+            fullWidth
+            variant="outlined"
+            margin="normal"
           />
 
           <Field
-          as={TextField}
-          name="email"
-          label="email"
-          fullWidth
-          variant="outlined"
-          margin="normal"
-          />
-          
-          <Field
-          as={TextField}
-          name="password"
-          label="password"
-          fullWidth
-          variant="outlined"
-          margin="normal"
+            as={TextField}
+            name="email"
+            label="email"
+            fullWidth
+            variant="outlined"
+            margin="normal"
           />
 
-          <Button sx={{mt:2, padding:"1rem"}} fullWidth type='submit' variant='contained'>Register</Button>
+          <Field
+            as={TextField}
+            name="password"
+            label="password"
+            fullWidth
+            variant="outlined"
+            margin="normal"
+            type="password"
+          />
+
+          <Field
+            fullWidth
+            margin="normal"
+            as={Select}
+            labelId="role-simple-select-label"
+            id="demo-simple-select"
+            name="role"
+          >
+            <MenuItem value={"ROLE_CUSTOMER"}>Customer</MenuItem>
+            <MenuItem value={"ROLE_RESTAURANT_OWNER"}>
+              Restaurant Owner
+            </MenuItem>
+          </Field>
+
+          <Button
+            sx={{ mt: 2, padding: "1rem" }}
+            fullWidth
+            type="submit"
+            variant="contained"
+          >
+            Register
+          </Button>
         </Form>
       </Formik>
-      <Typography variant='body2' align='center' sx={{mt:3}}>
+      <Typography variant="body2" align="center" sx={{ mt: 3 }}>
         If have an account already?
-        <Button size='small' onClick={()=>navigate("/account/register")}>login</Button>
+        <Button size="small" onClick={() => navigate("/account/register")}>
+          login
+        </Button>
       </Typography>
     </div>
-  )
-}
+  );
+};
