0649672fb50bb4dc83bb4b8e8e8b356d53f9fe38
VL.dilankarw <dilankarashmika10@gmail.com>
2024-09-15 16:20:50 +0530

    Auth Update

--- frontend/src/component/Auth/Auth.jsx
+++ frontend/src/component/Auth/Auth.jsx
@@ -2,6 +2,8 @@ import { Box, Modal } from '@mui/material';
 import React from 'react'
 import { style } from '../Cart/Cart';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { RegisterForm } from './RegisterForm';
+import { LoginForm } from './LoginForm';
 
 export const Auth = () => {
   const location = useLocation();
@@ -15,7 +17,7 @@ export const Auth = () => {
       location.pathname==="/account/register" || location.pathname==="/account/login"
     }>
       <Box sx={style}>
-        Hello
+        {location.pathname==="/account/register"?<RegisterForm/>:<LoginForm/>}
       </Box>
     </Modal>
     </>
