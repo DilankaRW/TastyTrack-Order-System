043b11eda82a3f16efdddba5064b79dedd56c19c
VL.dilankarw <dilankarashmika10@gmail.com>
2024-09-18 23:42:39 +0530

    loginUser method

--- frontend/src/component/State/Authentication/Action.js
+++ frontend/src/component/State/Authentication/Action.js
@@ -1,5 +1,5 @@
 import { type } from "@testing-library/user-event/dist/type"
-import { REGISTER_REQUEST, REGISTER_SUCCESS } from "./Reducer"
+import { LOGIN_REQUEST, LOGIN_SUCCESS, REGISTER_REQUEST, REGISTER_SUCCESS } from "./Reducer"
 import axios from "axios"
 import { API_URL } from "../../config/api"
 
@@ -14,6 +14,22 @@ export const registerUser=(reqData)=>async(dispatch)=>{
             reqData.navigate("/")
         }
         dispatch({type:REGISTER_SUCCESS,payload:data.jwt})
+    } catch (error){
+        console.log("error",error)
+    }
+}
+
+export const loginUser=(reqData)=>async(dispatch)=>{
+    dispatch({type:LOGIN_REQUEST})
+    try{
+        const {data}=await axios.post(`${API_URL}/auth/signin`,reqData.userData)
+        if (data.jwt)localStorage.setItem("jwt",data.jwt);
+        if(data.role==="ROLE_RESTAURANT_OWNER"){
+            reqData.navigate("/admin/restaurant")
+        } else {
+            reqData.navigate("/")
+        }
+        dispatch({type:LOGIN_SUCCESS,payload:data.jwt})
     } catch (error){
         console.log("error",error)
     }
