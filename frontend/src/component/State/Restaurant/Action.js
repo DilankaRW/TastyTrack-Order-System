519f0d8b24824d6c6d577412bfd2bafad0b03ea5
VL.dilankarw <dilankarashmika10@gmail.com>
2024-09-27 19:17:00 +0530

    getRestaurantById

--- frontend/src/component/State/Restaurant/Action.js
+++ frontend/src/component/State/Restaurant/Action.js
@@ -2,13 +2,16 @@ import {
   GET_ALL_RESTAURANT_FAILURE,
   GET_ALL_RESTAURANT_REQUEST,
   GET_ALL_RESTAURANT_SUCCESS,
+  GET_RESTAURANT_BY_ID_FAILURE,
+  GET_RESTAURANT_BY_ID_REQUEST,
+  GET_RESTAURANT_BY_ID_SUCCESS,
 } from "./ActionType";
 
 export const getAllRestaurantsAction = (token) => {
   return async (dispatch) => {
     dispatch({ type: GET_ALL_RESTAURANT_REQUEST });
     try {
-      const { data } = await Api.get("/api/restaurants", {
+      const { data } = await api.get("/api/restaurants", {
         headers: {
           Authorization: `Bearer ${token}`,
         },
@@ -18,6 +21,26 @@ export const getAllRestaurantsAction = (token) => {
     } catch (error) {
       console.log("catch error ", error);
       dispatch({ type: GET_ALL_RESTAURANT_FAILURE, payload: error });
+    }
+  };
+};
+
+export const getRestaurantById = (reqData) => {
+  return async (dispatch) => {
+    dispatch({ type: GET_RESTAURANT_BY_ID_REQUEST });
+    try {
+      const response = await api.get(
+        `/api/restaurants/${reqData.restaurantId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${reqData.jwt}`,
+          },
+        }
+      );
+      dispatch({ type: GET_RESTAURANT_BY_ID_SUCCESS, payload: response.data });
+    } catch (error) {
+      console.log("error ", error);
+      dispatch({ type: GET_RESTAURANT_BY_ID_FAILURE, payload: error });
     }
   };
 };
