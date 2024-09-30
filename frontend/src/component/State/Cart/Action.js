063266bf60647d92fa5041e870e85acaac7471d7
VL.dilankarw <dilankarashmika10@gmail.com>
2024-09-30 23:46:30 +0530

    Action - addItemToCart

--- frontend/src/component/State/Cart/Action.js
+++ frontend/src/component/State/Cart/Action.js
@@ -1,4 +1,7 @@
 import {
+  ADD_ITEM_TO_CART_FAILURE,
+  ADD_ITEM_TO_CART_REQUEST,
+  ADD_ITEM_TO_CART_SUCCESS,
   FIND_CART_FAILURE,
   FIND_CART_REQUEST,
   FIND_CART_SUCCESS,
@@ -35,6 +38,24 @@ export const getAllCartItems = (reqData) => {
       dispatch({ type: GET_ALL_CART_ITEMS_SUCCESS, payload: response.data });
     } catch (error) {
       dispatch({ type: GET_ALL_CART_ITEMS_FAILURE, payload: error });
+    }
+  };
+};
+
+export const addItemToCart = (reqData) => {
+  return async (dispatch) => {
+    dispatch({ type: ADD_ITEM_TO_CART_REQUEST });
+    try {
+      const { data } = await Api.put(`/api/cart/add`, reqData.cartItem, {
+        headers: {
+          Authorization: `Bearer ${reqData.token}`,
+        },
+      });
+      console.log("add item to cart ", data);
+      dispatch({ type: ADD_ITEM_TO_CART_SUCCESS, payload: data });
+    } catch (error) {
+      console.log("catch error ", error);
+      dispatch({ type: ADD_ITEM_TO_CART_FAILURE, payload: error.message });
     }
   };
 };
